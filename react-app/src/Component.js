5ea5a9678efcf8a6761ce26f7ead8de97400f7ff
StevenKligman <kligmasn@mail.uc.edu>
2018-06-26 08:53:48 -0400

    More complete draft design for navBar

--- react-app/src/Component.js
+++ react-app/src/Component.js
@@ -4,12 +4,25 @@ import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
 import FontIcon from 'material-ui/FontIcon';
 import Menu from 'material-ui/svg-icons/navigation/menu.js';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+
 
 const NavBarStyle = {
     display: 'flex',
     flexDirection: 'row',
-    backgroundColor: 'rgb(51, 204, 204)'
+    alignItems:'center',
+    backgroundColor: 'rgb(51, 204, 204)',
+    width:'100%'
 };
+const NavBarHeaderStyle = {
+    width:'80%',
+    textAlign: 'center',
+    fontSize:'24px'
+};
+const NavBarMenuStyle = {
+    width:'10%'
+}
 
 export default class NavBar extends React.Component {
 
@@ -23,16 +36,20 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div style={NavBarStyle}>
-        <FlatButton onClick={this.handleToggle}>
-          <Menu viewBox="0 0 19 19"/>
+        <FlatButton onClick={this.handleToggle} style={NavBarMenuStyle}>
+          <Menu viewBox="0 0 20 20" style={{marginTop:'2px'}}/>
         </FlatButton>
+        <div className="Header" style={NavBarHeaderStyle}>
+          Bark Bark
+        </div>
+        <Chip>
+          <Avatar icon={<Menu/>} />
+          FontIcon Avatar Chip
+        </Chip>
         <Drawer open={this.state.open}>
           <MenuItem>Menu Item</MenuItem>
           <MenuItem>Menu Item 2</MenuItem>
-          <FlatButton
-          label="Close"
-          onClick={this.handleToggle}
-          />
+          <FlatButton label="Close" onClick={this.handleToggle}/>
         </Drawer>
       </div>
     );
