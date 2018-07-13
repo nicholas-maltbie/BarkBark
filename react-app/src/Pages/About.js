97ec384eb6dc95bf39c846d5ff8a31d5b6932abc
Meghna Gupta <guptamg@mail.uc.edu>
2018-07-12 23:00:09 -0400

    Added terms and about page

---
+++ react-app/src/Pages/About.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const AboutPageScreenStyle ={
+    height: '90%',
+    width: '10%'
+}
+
+class AboutPage extends React.Component {
+    render(){
+        return(
+            <div className="Page" style={AboutPageScreenStyle}>
+                <h2> About Bark Bark </h2> 
+
+<h3> <b> If you want to learn more about how Bark Bark was designed and developed; Want to learn about the functionality of Bark Bark you are in the right place. </b></h3>
+
+<p>Bark Bark is a web application intended to be a local event finder where you can see what events people are barking at and bark at local events yourself. This social media app helps locate events, restaurants and other entertainment while providing additional information with embedded links (Eg. Yelp, Google Maps, Facebook).
+
+This location-based app will identify nearby points of interest by walking a user’s virtual dog and allowing users to prompt their dog to bark at interesting events. You can anonymously bark at any events integrated with the app (Eg. Yelp, Google Maps, Facebook) and custom events such as parties or gatherings for other users to see. Users will receive notifications of barks in categories they are interested in, and the location they are in at the time. 
+
+It serves as a great way to learn ratings about nearby places like restaurants or movie theaters and the services they provide like be able to rate the specific dishes within the restaurant. It becomes tedious and exhausting to sort through reviews and pictures to learn about specific locations or events thus this is an easier ratings system as there are no words or pictures but just simple emojis that describe the emotion. </p>
+
+
+<h2> <b> Mission </b> </h2>  
+
+<p>Develop a free application that can be used to help connect people to local events and help rank and rate local eateries and destinations. After testing and ensuring compatibility with Google Maps, the project should be made available in the App store for public use. The data collected by user ratings and barks will be stored in a database to generate ratings and rankings for similarly themed locations and events. </p>
+ 
+<p>The project will be developed as a Web Interface so that it is compatible with both Android and iOS operating systems. The application will also not support the option to add text or pictures, the primary means of communication and expression on the application will be through the use of emojis. </p>
+
+<h2> <b> Objective </b> </h2> 
+
+<ol>
+<li>The purpose is to be an event finder and rating application for local events, restaurants and other entertainment venues </li>
+<li>An app where users have a virtual dog</li>
+<li>Imagine you are walking your dog, your dog barks at something interesting and you share what your dog barks at</li>
+<li>A bark is a ping to tell others of events and locations you are interested in</li>
+<li>Users can have their pet dog bark at (pin) anything within a nearby radius </li>
+<li>Simplified rating system to find local events happening currently </li>
+<li>Social app where you and others can share and bark at local upcoming and ongoing events</li>
+<li>Maintain history of a person’s barks </li>
+<li>Create an individual dog avatar and use custom dog emojis </li>
+</ol>
+<h2> Our Team </h2> 
+
+<p>The Bark Bark project was designed as a part of an assignment for a Software Engineering course at the University of Cincinnati in the Summer of 2018. The team comprised of 5 members that were responsible for the designing, coding, development and maintenance of the website based location application. The team spent three months (duration of the semester) to build this application for market use.</p>
+
+<p> <b>If you have any concerns, suggestions, queries regarding Bark Bark please feel free to contact us using the Support Page. </b> </p>
+            </div>
+        )
+    }
+}
+
+export default AboutPage;
