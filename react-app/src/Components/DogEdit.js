353fdbf245285fe6ed3bb5c0a0e9e96fad8c9bd9
Nick Maltbie <nick.dmalt@gmail.com>
2018-07-15 13:41:14 -0400

    Removed test image

--- react-app/src/Components/DogEdit.js
+++ react-app/src/Components/DogEdit.js
@@ -8,7 +8,6 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import testPicture from '../Static/715(1).jpg';
 
 const valueSelectorStyle = {
   display: 'flex',
