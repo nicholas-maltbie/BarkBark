59bd0fed50d1eb2b76df2becbb7b60f3b636022e
StevenKligman <kligmasn@mail.uc.edu>
2018-07-15 13:19:49 -0400

    Added dog edit pop up

--- react-app/src/Components/DogEdit.js
+++ react-app/src/Components/DogEdit.js
@@ -14,10 +14,11 @@ const valueSelectorStyle = {
   display: 'flex',
   flexDirection: 'column',
   width:'500px',
-  height:'300px'
+  height:'300px',
+  padding: '5px'
 }
 const valueSelectorBarStyle = {
-  paddingTop: '5px'
+  paddingTop: '5px',
 }
 const dogPreviewStyle = {
   width:'500px',
@@ -26,8 +27,9 @@ const dogPreviewStyle = {
 const dogEditWindow = {
   display:'flex',
   flexDirection:'column',
-  width:'600px',
-  height:'8000px'
+  alignItems:'center',
+  width:'510px',
+  height:'800px'
 }
 const dogPreviewAvatarStyle = {
   width:'100%',
@@ -62,75 +64,75 @@ class DogEdit extends React.Component {
     
     return (
       <div style={dogEditWindow}>
-      <div className="dogPreviewStyle" style={{backgroundColor: this.state.backgroundColorOptions[this.state.backgroundValue]}}>
-        <Card>
-            <div className="dogPreviewBGStyle"/>
-            <div style={dogPreviewAvatarStyle}>
-            </div>
-        </Card>
-      </div>
-      <div style={valueSelectorStyle}>
-        <AppBar position="static" color="default" style={valueSelectorBarStyle}>
-          <Typography> Background Color </Typography>
-          <Tabs
-            value={this.state.backgroundValue}
-            onChange={(event, value) => this.handleChange(event, value, 'BGColor')}
-            scrollable
-            scrollButtons="auto"
-          >
-            <Tab label="Blue" style={{backgroundColor:'blue', color:'white'}}/>
-            <Tab label="Green" style={{backgroundColor:'Green', color:'white'}}/>
-            <Tab label="Yellow" style={{backgroundColor:'Yellow', color:'white'}}/>
-            <Tab label="Orange" style={{backgroundColor:'Orange', color:'white'}}/>
-            <Tab label="Violet" style={{backgroundColor:'Violet', color:'white'}}/>
-          </Tabs>
-        </AppBar>
-        <AppBar position="static" color="default" style={valueSelectorBarStyle}>
-          <Typography> Fur Color </Typography>
-          <Tabs
-            value={this.state.furColorValue}
-            onChange={(event, value) => this.handleChange(event, value, 'FurColor')}
-            scrollable
-            scrollButtons="auto"
-          >
-            <Tab label="Blue" style={{backgroundColor:'blue', color:'white'}}/>
-            <Tab label="Green" style={{backgroundColor:'Green', color:'white'}}/>
-            <Tab label="Yellow" style={{backgroundColor:'Yellow', color:'white'}}/>
-            <Tab label="Orange" style={{backgroundColor:'Orange', color:'white'}}/>
-            <Tab label="Violet" style={{backgroundColor:'Violet', color:'white'}}/>
-          </Tabs>
-        </AppBar>
-        <AppBar position="static" color="default" style={valueSelectorBarStyle}>
-          <Typography> Eye Color </Typography>
-          <Tabs
-            value={this.state.eyeColorValue}
-            onChange={(event, value) => this.handleChange(event, value, 'EyeColor')}
-            scrollable
-            scrollButtons="auto"
-          >
-            <Tab label="Blue" style={{backgroundColor:'blue', color:'white'}}/>
-            <Tab label="Green" style={{backgroundColor:'Green', color:'white'}}/>
-            <Tab label="Yellow" style={{backgroundColor:'Yellow', color:'white'}}/>
-            <Tab label="Orange" style={{backgroundColor:'Orange', color:'white'}}/>
-            <Tab label="Violet" style={{backgroundColor:'Violet', color:'white'}}/>
-          </Tabs>
-        </AppBar>
-        <AppBar position="static" color="default" style={valueSelectorBarStyle}>
-          <Typography> Breed </Typography>
-          <Tabs
-            value={this.state.breedValue}
-            onChange={(event, value) => this.handleChange(event, value, 'Breed')}
-            scrollable
-            scrollButtons="auto"
-          >
-            <Tab label="Poodle"/>
-            <Tab label="Labrador" />
-            <Tab label="Pitbull" />
-            <Tab label="German Shepard" />
-            <Tab label="Husky" />
-          </Tabs>
-        </AppBar>
-      </div>
+        <div className="dogPreviewStyle" style={{backgroundColor: this.state.backgroundColorOptions[this.state.backgroundValue]}}>
+          <Card>
+              <div className="dogPreviewBGStyle"/>
+              <div style={dogPreviewAvatarStyle}>
+              </div>
+          </Card>
+        </div>
+        <div style={valueSelectorStyle}>
+          <AppBar position="static" color="default" style={valueSelectorBarStyle}>
+            <Typography> Background Color </Typography>
+            <Tabs
+              value={this.state.backgroundValue}
+              onChange={(event, value) => this.handleChange(event, value, 'BGColor')}
+              scrollable
+              scrollButtons="auto"
+            >
+              <Tab label="Blue" style={{backgroundColor:'blue', color:'white'}}/>
+              <Tab label="Green" style={{backgroundColor:'Green', color:'white'}}/>
+              <Tab label="Yellow" style={{backgroundColor:'Yellow', color:'white'}}/>
+              <Tab label="Orange" style={{backgroundColor:'Orange', color:'white'}}/>
+              <Tab label="Violet" style={{backgroundColor:'Violet', color:'white'}}/>
+            </Tabs>
+          </AppBar>
+          <AppBar position="static" color="default" style={valueSelectorBarStyle}>
+            <Typography> Fur Color </Typography>
+            <Tabs
+              value={this.state.furColorValue}
+              onChange={(event, value) => this.handleChange(event, value, 'FurColor')}
+              scrollable
+              scrollButtons="auto"
+            >
+              <Tab label="Blue" style={{backgroundColor:'blue', color:'white'}}/>
+              <Tab label="Green" style={{backgroundColor:'Green', color:'white'}}/>
+              <Tab label="Yellow" style={{backgroundColor:'Yellow', color:'white'}}/>
+              <Tab label="Orange" style={{backgroundColor:'Orange', color:'white'}}/>
+              <Tab label="Violet" style={{backgroundColor:'Violet', color:'white'}}/>
+            </Tabs>
+          </AppBar>
+          <AppBar position="static" color="default" style={valueSelectorBarStyle}>
+            <Typography> Eye Color </Typography>
+            <Tabs
+              value={this.state.eyeColorValue}
+              onChange={(event, value) => this.handleChange(event, value, 'EyeColor')}
+              scrollable
+              scrollButtons="auto"
+            >
+              <Tab label="Blue" style={{backgroundColor:'blue', color:'white'}}/>
+              <Tab label="Green" style={{backgroundColor:'Green', color:'white'}}/>
+              <Tab label="Yellow" style={{backgroundColor:'Yellow', color:'white'}}/>
+              <Tab label="Orange" style={{backgroundColor:'Orange', color:'white'}}/>
+              <Tab label="Violet" style={{backgroundColor:'Violet', color:'white'}}/>
+            </Tabs>
+          </AppBar>
+          <AppBar position="static" color="default" style={valueSelectorBarStyle}>
+            <Typography> Breed </Typography>
+            <Tabs
+              value={this.state.breedValue}
+              onChange={(event, value) => this.handleChange(event, value, 'Breed')}
+              scrollable
+              scrollButtons="auto"
+            >
+              <Tab label="Poodle"/>
+              <Tab label="Labrador" />
+              <Tab label="Pitbull" />
+              <Tab label="German Shepard" />
+              <Tab label="Husky" />
+            </Tabs>
+          </AppBar>
+        </div>
       </div>
     );
   }
