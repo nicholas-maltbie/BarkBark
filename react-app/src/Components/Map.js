9ca3045b09cde31b7a3aa2c4b6a553c9eb87ad32
Nick Maltbie <nick.dmalt@gmail.com>
2018-07-15 13:57:20 -0400

    Map now updated based on user location
    Will do one update when user launches app

--- react-app/src/Components/Map.js
+++ react-app/src/Components/Map.js
@@ -6,18 +6,20 @@ import React, { Component } from 'react';
 const google = window.google;
 
 export const getLocation = () => {
-    const geolocation = navigator.geolocation;
+  const geolocation = navigator.geolocation;
 
-    const location = new Promise((resolve, reject) => {
-        if (!geolocation) {
-            reject(new Error('Not Supported'));
-        }
+  const location = new Promise((resolve, reject) => {
+    if (!geolocation) {
+      reject(new Error('Not Supported'));
+    }
 
-    geolocation.getCurrentPosition((position) => {
-        resolve(position);
-    }, () => {
-        reject (new Error('Permission denied'));
-    });
+  geolocation.getCurrentPosition((position) => {
+    resolve(position);
+  }, () => {
+    reject (
+      alert("This app requires permission to share your location, https://support.google.com/chrome/answer/142065?hl=en")
+    );
+  });
   });
 
   return location
@@ -27,13 +29,22 @@ class Map extends Component {
   
   constructor(props) {
     super(props);
+    
+    this.updateLocation = this.updateLocation.bind(this);
+  }
+  
+  updateLocation(newPos) {
+    console.log(newPos)
+    this.map.setCenter({lat: newPos.coords.latitude, lng: newPos.coords.longitude})
   }
   
   componentDidMount () {
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      zoom: 14
     });
+    
+    getLocation().then(this.updateLocation)
   }
   
   render() {
