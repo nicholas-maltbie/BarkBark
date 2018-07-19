fd325cf3d8198b4b7a73b6f2a9098b83357654d9
Nick Maltbie <nick.dmalt@gmail.com>
2018-07-19 12:29:37 -0400

    Made drawer close on click link

--- react-app/src/Components/NavBar.js
+++ react-app/src/Components/NavBar.js
@@ -48,6 +48,7 @@ class NavBar extends React.Component {
 
     this.handleLink = this.handleLink.bind(this);
     this.signOut = this.signOut.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
@@ -65,6 +66,13 @@ class NavBar extends React.Component {
     firebase.auth().signOut()
     e.preventDefault();
     this.props.history.push('/');
+  }
+  
+  toggleDrawer(otherFn) {
+    return (e) => {
+      this.props.handleDrawerToggle()
+      otherFn(e)
+    }
   }
 
   render() {
@@ -87,14 +95,14 @@ class NavBar extends React.Component {
           icon={ <Person color={navButtonColor}/>}/>
 		  <div backgroundColor = "#000">
         <Drawer open={!this.props.drawerClose}>
-          <MenuItem onClick={this.signOut}>Sign Out</MenuItem>
-          <MenuItem onClick={this.handleLink}>Home</MenuItem>
-          <MenuItem onClick={this.handleLink}>Map</MenuItem>
-          <MenuItem onClick={this.handleLink}>Profile</MenuItem>
-          <MenuItem onClick={this.handleLink}>Help</MenuItem>
-          <MenuItem onClick={this.handleLink}>About Us</MenuItem>
-          <MenuItem onClick={this.handleLink}>Terms Of Use</MenuItem>
-          <MenuItem onClick={this.handleLink}>Contact Us</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.signOut)}>Sign Out</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Home</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Map</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Profile</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Help</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>About Us</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Terms Of Use</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Contact Us</MenuItem>
         </Drawer>
 		</div>
       </div>
@@ -115,11 +123,11 @@ class NavBar extends React.Component {
         <FlatButton 
           style={NavBarButtonStyle}/>
         <Drawer open={!this.props.drawerClose}>
-          <MenuItem onClick={this.handleLink}>Sign In</MenuItem>
-          <MenuItem onClick={this.handleLink}>Help</MenuItem>
-          <MenuItem onClick={this.handleLink}>Contact Us</MenuItem>
-          <MenuItem onClick={this.handleLink}>Terms Of Use</MenuItem>
-          <MenuItem onClick={this.handleLink}>About Us</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Sign In</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Help</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Contact Us</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>Terms Of Use</MenuItem>
+          <MenuItem onClick={this.toggleDrawer(this.handleLink)}>About Us</MenuItem>
         </Drawer>
       </div>
       )
